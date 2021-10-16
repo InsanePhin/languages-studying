a0a53a045ac8be6e632aa573d7bfcc59d4b5c313
InsanePhin <insanephin@gmail.com>
2021-10-16 14:48:02 +0900

    될거야

--- profile/src/index.js
+++ profile/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Time from './Time';
 import reportWebVitals from './reportWebVitals';
 
+function Time() {
+    return (
+        <div className="Time">
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Time />
-  </React.StrictMode>,
-  document.getElementById('time')
+    Time,
+    document.getElementById('time')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
 setInterval(Time, 1000);
