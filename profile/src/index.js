bac645a355aa9b4a15dc7096c61bf85a57bd7ac1
InsanePhin <insanephin@gmail.com>
2021-10-16 14:56:51 +0900

    이건

--- profile/src/index.js
+++ profile/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 
-function tick() { 
-    const element = ( 
-        <div> <h1>Hello, world!</h1> 
-        <h2>It is {new Date().toLocaleTimeString()}.</h2> 
-        </div> 
-    );
-    ReactDOM.render(
-        tick.element,
-        document.getElementById('time')
-    );
-}   
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('time'));
+}
 
-reportWebVitals(console.log);
 setInterval(tick, 1000);
